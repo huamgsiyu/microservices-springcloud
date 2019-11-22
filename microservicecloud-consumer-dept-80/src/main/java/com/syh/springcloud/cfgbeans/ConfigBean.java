900555b01603dba4bb6ca0579c95426ef504d82b
huamgsiyu <909246273@qq.com>
2019-11-22 21:38:58 +0800

    Ribbon提供了7种算法，修改Ribbon的默认算法

--- microservicecloud-consumer-dept-80/src/main/java/com/syh/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/syh/springcloud/cfgbeans/ConfigBean.java
@@ -4,6 +4,9 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RetryRule;
  
 @Configuration
 public class ConfigBean
@@ -13,6 +16,12 @@ public class ConfigBean
     public RestTemplate getRestTemplate()
     {
          return new RestTemplate();
+    }
+    
+    //显式指定Ribbon使用的负载均衡算法，如果不指定，默认使用的是RoundRobinRule——轮询算法
+    @Bean
+    public IRule myRule () {
+    	return new RetryRule();	//指定使用随机算法
     }
 }
 
