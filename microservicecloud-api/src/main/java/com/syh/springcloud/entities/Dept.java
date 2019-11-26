bc6da1bab6924e350319b7d5f1c59b4766d36a0b
huamgsiyu <909246273@qq.com>
2019-11-26 10:18:18 +0800

    111

--- microservicecloud-api/src/main/java/com/syh/springcloud/entities/Dept.java
+++ microservicecloud-api/src/main/java/com/syh/springcloud/entities/Dept.java
@@ -1,33 +1,16 @@
 package com.syh.springcloud.entities;
- 
+
 import java.io.Serializable;
 
-public class Dept implements Serializable //必须序列化
+public class Dept implements Serializable// entity --orm--- db_table
 {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private Long  deptno;   //主键
-	private String  dname;   //部门名称
-	private String  db_source;//来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
-	
-	
-	public Dept() {
-		super();
-	}
-	
-	public Dept(Long deptno, String dname, String db_source) {
-		super();
-		this.deptno = deptno;
-		this.dname = dname;
-		this.db_source = db_source;
-	}
-
-	@Override
-	public String toString() {
-		return "Dept [deptno=" + deptno + ", dname=" + dname + ", db_source=" + db_source + "]";
-	}
+	private Long 	deptno; // 主键
+	private String 	dname; // 部门名称
+	private String 	db_source;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
 	public Long getDeptno() {
 		return deptno;
 	}
@@ -46,8 +29,16 @@ public class Dept implements Serializable //必须序列化
 	public void setDb_source(String db_source) {
 		this.db_source = db_source;
 	}
-	
+	@Override
+	public String toString() {
+		return "Dept [deptno=" + deptno + ", dname=" + dname + ", db_source=" + db_source + "]";
+	}
+	public Dept(Long deptno, String dname, String db_source) {
+		super();
+		this.deptno = deptno;
+		this.dname = dname;
+		this.db_source = db_source;
+	}
+	public Dept() {
+	}
 }
- 
- 
-
--- microservicecloud/microservicecloud-api/src/main/java/com/syh/springcloud/entities/Dept.java
+++ microservicecloud/microservicecloud-api/src/main/java/com/syh/springcloud/entities/Dept.java
@@ -1,33 +1,24 @@
 package com.syh.springcloud.entities;
- 
+
 import java.io.Serializable;
 
-public class Dept implements Serializable //必须序列化
-{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private Long  deptno;   //主键
-	private String  dname;   //部门名称
-	private String  db_source;//来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
-	
-	
-	public Dept() {
-		super();
-	}
-	
-	public Dept(Long deptno, String dname, String db_source) {
-		super();
-		this.deptno = deptno;
-		this.dname = dname;
-		this.db_source = db_source;
-	}
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 
-	@Override
-	public String toString() {
-		return "Dept [deptno=" + deptno + ", dname=" + dname + ", db_source=" + db_source + "]";
-	}
+
+@NoArgsConstructor
+//@AllArgsConstructor
+@Data
+@Accessors(chain=true)
+public class Dept implements Serializable// entity --orm--- db_table
+{
+	private Long 	deptno; // 主键
+	private String 	dname; // 部门名称
+	private String 	db_source;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
 	public Long getDeptno() {
 		return deptno;
 	}
@@ -46,8 +37,17 @@ public class Dept implements Serializable //必须序列化
 	public void setDb_source(String db_source) {
 		this.db_source = db_source;
 	}
-	
+	@Override
+	public String toString() {
+		return "Dept [deptno=" + deptno + ", dname=" + dname + ", db_source=" + db_source + "]";
+	}
+	public Dept(Long deptno, String dname, String db_source) {
+		super();
+		this.deptno = deptno;
+		this.dname = dname;
+		this.db_source = db_source;
+	}
+	public Dept() {
+		super();
+	}
 }
- 
- 
-
